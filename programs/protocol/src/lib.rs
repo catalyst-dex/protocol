e2233015f6200459246c0f37453cff3305e44d89
Ejiofor Celestine Chinaza <121381551+CyberVerse2@users.noreply.github.com>
2024-11-27 08:10:40 +0100

    initialized all files

---
+++ programs/protocol/src/lib.rs
@@ -0,0 +1,47 @@
+#![allow(clippy::result_large_err)]
+
+use anchor_lang::prelude::*;
+
+mod constants;
+mod errors;
+mod instructions;
+mod state;
+// Remove or rename either `state.rs` or `mod.rs` to remove the ambiguity
+
+// Set the correct key here
+declare_id!("hp4fshAkzdfq65PLWckYsXnNAwofWF6WMvywQNAfCfK");
+
+#[program]
+pub mod catalyst_protocol {
+    pub use super::instructions::*;
+    use super::*;
+
+    pub fn create_amm(ctx: Context<CreateAmm>, id: Pubkey, fee: u16) -> Result<()> {
+        instructions::create_amm(ctx, id, fee)
+    }
+
+    pub fn create_pool(ctx: Context<CreatePool>) -> Result<()> {
+        instructions::create_pool(ctx)
+    }
+
+    pub fn deposit_liquidity(
+        ctx: Context<DepositLiquidity>,
+        amount_a: u64,
+        amount_b: u64,
+    ) -> Result<()> {
+        instructions::deposit_liquidity(ctx, amount_a, amount_b)
+    }
+
+    pub fn withdraw_liquidity(ctx: Context<WithdrawLiquidity>, amount: u64) -> Result<()> {
+        instructions::withdraw_liquidity(ctx, amount)
+    }
+
+    pub fn swap_exact_tokens_for_tokens(
+        ctx: Context<SwapExactTokensForTokens>,
+        swap_a: bool,
+        input_amount: u64,
+        min_output_amount: u64,
+    ) -> Result<()> {
+        instructions::swap_exact_tokens_for_tokens(ctx, swap_a, input_amount, min_output_amount)
+    }
+}
